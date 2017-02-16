a95e126d89f0421e44c936d274c6f4eae7df5995
Porco-Rosso <Porco-Rosso@users.noreply.github.com>
2017-02-16 13:40:05 +0100

    Initial Commit

---
+++ matrix.widget/matrix.js
@@ -0,0 +1,14 @@
+command: "",
+
+refreshFrequency: 138000,
+
+render: function (output) {
+  return "<iframe width=\"1880\" height=\"1250\" src=\"matrix.widget/Matrix.html\" frameborder=\"0\" allowfullscreen></iframe>";
+},
+
+style: "        \n\
+  top: 0px     \n\
+  left: 0px    \n\
+  z-index: -100 \n\
+  opacity: 0.5 \n\
+"
