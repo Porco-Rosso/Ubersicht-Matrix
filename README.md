aca4941304daccdad3a8fa106730540d2c4fab38
Porco-Rosso <Porco-Rosso@users.noreply.github.com>
2017-02-17 15:46:26 +0100

    Ready for release!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ubersicht-Matrix
+This widget allows to set a matrix-like falling code visualization to your desktop background.  
+<b>Warning:</b> This might be CPU intensive!
+
+For use with Übersicht ==> http://tracesof.net/uebersicht/
+
+
+![Demo Screenshot](screenshot.png)
+
+----
+Version 1.0.0 - *Initial release*
--- matrix.widget/matrix.js
+++ matrix.widget/matrix.js
@@ -1,3 +1,9 @@
+// # For use with Übersicht ==> http://tracesof.net/uebersicht/
+// # Coded by Porco-Rosso ==> https://github.com/Porco-Rosso
+// # from Stereostance.com
+// # v1.0.0
+// # GNU GPL V2
+
 command: "",
 
 refreshFrequency: 138000,
@@ -6,6 +12,7 @@ render: function (output) {
   return "<iframe width=\"1880\" height=\"1250\" src=\"matrix.widget/Matrix.html\" frameborder=\"0\" allowfullscreen></iframe>";
 },
 
+// Adjust the opacity value if you would still like to see your wallpaper behind the visualization
 style: "        \n\
   top: 0px     \n\
   left: 0px    \n\
